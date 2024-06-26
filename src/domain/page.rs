96f2b6319698e3eb5670df3a031083e27d6c32b7
delgado <alex.o.delgado@gmail.com>
2024-06-26 01:05:11 -0400

    Update routes to address move of search into its base moduels

--- src/domain/page.rs
+++ src/domain/page.rs
@@ -1,11 +1,12 @@
 use crate::{
     auth::has_ui_access,
     domain::core::{domain_add, Domain, DomainParam},
+    index::Navbar,
 };
 use askama::Template;
 use poem::{
     error::InternalServerError,
-    handler, post,
+    get, handler, post,
     session::Session,
     web::{Data, Form, Html},
     IntoResponse, Request, Response, Route,
@@ -87,7 +88,41 @@ pub async fn domain_form(
     Ok(Html(domain_form).into_response())
 }
 
+/// Tempate for Domain Search Page
+#[derive(Template)]
+#[template(path = "domain/page/domain_search.html")]
+struct DomainSearch {
+    navbar: Navbar,
+    domain_add: DomainForm,
+}
+
+/// Sign in page
+#[handler]
+fn domain_search(session: &Session, req: &Request) -> Result<Response, poem::Error> {
+    // If we have the username from the cookies, do they have access?
+    let username: Option<String> = session.get("username");
+    let has_access: bool = match &username {
+        Some(username) => has_ui_access(username, req),
+        None => false,
+    };
+
+    // Render HTML
+    let domain_search: String = DomainSearch {
+        navbar: Navbar { username },
+        domain_add: DomainForm {
+            error: None,
+            has_access,
+        },
+    }
+    .render()
+    .map_err(InternalServerError)?;
+
+    Ok(Html(domain_search).into_response())
+}
+
 /// Provide routs for the API endpoints
 pub fn route() -> Route {
-    Route::new().at("/", post(domain_form))
+    Route::new()
+        .at("/domain", post(domain_form))
+        .at("/search", get(domain_search))
 }
