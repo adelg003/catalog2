03e931267fab4c8ea30edf88176ed7bbf566b83c
delgado <alex.o.delgado@gmail.com>
2024-08-19 02:01:58 -0400

    Add support for graph with multiple nodes selected

--- src/graph/core.rs
+++ src/graph/core.rs
@@ -8,7 +8,7 @@ use sqlx::{FromRow, Postgres, Transaction};
 use std::fmt;
 
 /// Nodes that will make up our graph response
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Clone, Debug, PartialEq, Serialize)]
 pub struct Node {
     r#type: DependencyType,
     id: i32,
@@ -60,8 +60,8 @@ impl Edge {
 
 const DISTANCE_MAX: u32 = 100;
 
-/// Build the dependency graph
-pub async fn graph_dependencies(
+/// Build the dependency graph for a single node
+pub async fn graph_node_dependencies(
     tx: &mut Transaction<'_, Postgres>,
     node_type: &DependencyType,
     node_name: &str,
@@ -92,37 +92,37 @@ pub async fn graph_dependencies(
         .map_err(InternalServerError)?;
 
     // Isolate build graph logic to limit mutability
-    let dependency_flow: Dag = {
+    let dag: Dag = {
         // Graph we will be working with
-        let mut dependency_flow: Dag = Graph::new();
+        let mut dag: Dag = Graph::new();
 
         // Add root node
-        dependency_flow.add_node(Node {
+        dag.add_node(Node {
             r#type: *node_type,
             id: node_id,
             name: node_name.to_string(),
         });
 
         // Add edges
-        for edge in edges {
+        edges.iter().for_each(|edge: &Edge| {
             // Get Nodes
             let source_node: Node = edge.source_node();
             let dest_node: Node = edge.dest_node();
 
             // Search to see if node already exists. If not, add new node.
-            let source_index: NodeIndex = upsert_node(&mut dependency_flow, source_node);
-            let dest_index: NodeIndex = upsert_node(&mut dependency_flow, dest_node);
+            let source_index: NodeIndex = upsert_node(&mut dag, &source_node);
+            let dest_index: NodeIndex = upsert_node(&mut dag, &dest_node);
 
             // Add edge
-            dependency_flow.update_edge(source_index, dest_index, 1);
-        }
+            dag.update_edge(source_index, dest_index, 1);
+        });
 
-        dependency_flow
+        dag
     };
 
     // Is our dag really a dag? If not, raise error
-    match is_cyclic_directed(&dependency_flow) {
-        false => Ok(dependency_flow),
+    match is_cyclic_directed(&dag) {
+        false => Ok(dag),
         true => Err(poem::Error::from_string(
             "Dependency flow is cyclical and cannot be rendered as a DAG",
             StatusCode::UNPROCESSABLE_ENTITY,
@@ -130,9 +130,70 @@ pub async fn graph_dependencies(
     }
 }
 
+/// Build the dependency graph for multiple nodes
+pub async fn graph_many_node_dependencies(
+    tx: &mut Transaction<'_, Postgres>,
+    node_type: &DependencyType,
+    node_names: &[String],
+    source_distance: &Option<u32>,
+    dest_distance: &Option<u32>,
+) -> Result<Dag, poem::Error> {
+    // Collect dag per node
+    let dags: Vec<Dag> = {
+        let mut dags: Vec<Dag> = Vec::new();
+
+        for node_name in node_names {
+            let dag: Dag =
+                graph_node_dependencies(tx, node_type, node_name, source_distance, dest_distance)
+                    .await?;
+
+            dags.push(dag);
+        }
+        dags
+    };
+
+    // Combine all the Dag Graphs into one
+    let dag: Dag = {
+        // Working dag we will copy all nodes and edges to
+        let mut dag: Dag = Graph::new();
+
+        // Append the nodes and edges for each dag to the output dag
+        dags.iter().for_each(|working_dag: &Dag| {
+            graph_append(&mut dag, working_dag);
+        });
+
+        dag
+    };
+
+    Ok(dag)
+}
+
 /// Search to see if node already exists. If not, add new node.
-fn upsert_node(dag: &mut Dag, node: Node) -> NodeIndex {
+fn upsert_node(dag: &mut Dag, node: &Node) -> NodeIndex {
     dag.node_indices()
-        .find(|index| dag[*index] == node)
-        .unwrap_or_else(|| dag.add_node(node))
+        .find(|index| &dag[*index] == node)
+        .unwrap_or_else(|| dag.add_node(node.clone()))
+}
+
+/// Append all the nodes and edges of a graph to a different graph. If matching nodes / edges, do
+/// not duplicate the node / edges
+fn graph_append(main_dag: &mut Dag, append_dag: &Dag) {
+    // Append all the nodes to the main dag
+    append_dag.raw_nodes().iter().for_each(|raw_node| {
+        upsert_node(main_dag, &raw_node.weight);
+    });
+
+    // Append all the edges to the main dag
+    append_dag.raw_edges().iter().for_each(|raw_edge| {
+        // With the location in the append_dag, pull the node value
+        let source_node: &Node = &append_dag[raw_edge.source()];
+        let dest_node: &Node = &append_dag[raw_edge.target()];
+
+        // Find the location of the node value in the main_dag
+        let source_index: NodeIndex = upsert_node(main_dag, source_node);
+        let dest_index: NodeIndex = upsert_node(main_dag, dest_node);
+
+        // Add the new edge with the index addressed for the main_dag and not the append_dag
+        main_dag.update_edge(source_index, dest_index, 1);
+    });
 }
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -2,4 +2,4 @@ mod api;
 mod core;
 mod db;
 
-pub use crate::graph::{api::GraphApi, core::graph_dependencies};
+pub use crate::graph::{api::GraphApi, core::graph_node_dependencies};
